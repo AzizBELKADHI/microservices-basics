fe4b562663b73f5b77638be7bef65b9145b22ecb
azizbelkadhi <azizbelkadhi.ab@gmail.com>
2022-05-17 18:04:35 +0200

    Added Config Service

---
+++ config-service/src/test/java/com/beautycenter/ms/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beautycenter.ms.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
